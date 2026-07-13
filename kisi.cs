9d028fa898b8e42845ba9463c21d78e1332c7557
yigit pirgayipoglu <ypirgayipoglu24@posta.pau.edu.tr>
2026-07-13 23:46:47 +0300

    Add Kisi class with properties and method

---
+++ kisi.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_3
+{
+    internal class Kisi
+    {
+        public string Ad;
+        public string Soyad;
+        public int Yas;
+
+        public void Ozellikler()
+        {
+            Console.WriteLine("{0}\n{1}\n{2}",Ad,Soyad,Yas);
+        }
+    }
+}
